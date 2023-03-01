369e982c37b350a98ab84b87e10173e479dfcd3f
Abhay <kabhay849@gmail.com>
2023-03-01 22:16:52 +0530

    Redux app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BalanceButton from './components/BalanceButton';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BalanceButton/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BalanceButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BalanceButton = () => {
+  return (
+    <div>
+ 
+ <button className='btn btn-primary'>-</button>
+ <h2 className='d-inline m-5 lh-1'>Add to Cart</h2>
+ <button className='btn btn-primary'>+</button>
+
+
+
+    </div>
+  )
+}
+
+export default BalanceButton
